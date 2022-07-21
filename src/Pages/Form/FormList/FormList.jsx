201e9b4bcdb2d2328d9a3057a52b9ab4cecb0d4f
hyunjung <hazelc3837@gmail.com>
2022-07-21 16:25:02 +0900

    add:formlist layout 구현 중

--- src/Pages/Form/FormList/FormList.jsx
+++ src/Pages/Form/FormList/FormList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import FormListBox from "./FormListBox/FormListBox";
+import styled from "styled-components";
 
 const FormList = () => {
   return (
-    <div>
-      <div>FormList</div>
-    </div>
+    <FormListContainer>
+      <FormListBox />
+    </FormListContainer>
   );
 };
 
 export default FormList;
+
+const FormListContainer = styled.div`
+  width: 100%;
+`;
